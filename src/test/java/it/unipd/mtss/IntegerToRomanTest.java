d13ca5a6b5d19c5f5d23d33de635028e8ac841b4
AlexVedovato <alex.vedovato@gmail.com>
2023-05-05 22:12:54 +0200

    Implementata conversione primi tre numeri close #4

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,6 +1,10 @@
 package it.unipd.mtss;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.HashMap;
 
 import org.junit.Test;
 
@@ -8,8 +12,61 @@ public class IntegerToRomanTest
 {
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void first3NumberConversion(){
+        assertEquals(IntegerToRoman.convert(1),"I");
+        assertEquals(IntegerToRoman.convert(2),"II");
+        assertEquals(IntegerToRoman.convert(3),"III");
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void overRangeThrowsIllegalArgumentException() {
+        IntegerToRoman.convert(1001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void underRangeThrowsIllegalArgumentException() {
+        IntegerToRoman.convert(0);
+    }
+
+    @Test(timeout = 200) 
+    public void performance() {
+        IntegerToRoman.convert(888); //888 è il numero romano più lungo tra 1 e 1000
+    }
+
+    @Test
+    public void checkInverseRelationship(){
+        //arrange
+        Random rand = new Random();
+        int min = 1;
+        int max = 1000;
+        int randomNumber = rand.nextInt(max - min + 1) + min;
+        Map<Character, Integer> romanNumerals = new HashMap<Character, Integer>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+        romanNumerals.put('L', 50);
+        romanNumerals.put('C', 100);
+        romanNumerals.put('D', 500);
+        romanNumerals.put('M', 1000);
+        int result = 0;
+        int prevValue = 0;
+
+        //act
+        String s=IntegerToRoman.convert(randomNumber);
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curValue = romanNumerals.get(s.charAt(i));
+            if (curValue < prevValue) {
+                result -= curValue;
+            } else {
+                result += curValue;
+            }
+            prevValue = curValue;
+        }
+
+        //assert
+        assertEquals(randomNumber,result);
+    }
+
+
+
 }
